c94c3d7eb86aa7aab17c5b629f050583fd76e061
Débora Picado <dbpicado@gmail.com>
2021-04-11 02:56:06 +0000

    Primer archivo

--- src/models.py
+++ src/models.py
@@ -2,11 +2,41 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+class Personajes(Base):
+    __tablename__ = 'personjes'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    personaje = Column(String(250), nullable=False)
+    genero = Column(String(250), nullable=False)
+    color_de_ojos = Column(String(250), nullable=False)
+
+class Planetas(Base):
+    __tablename__ = 'planetas'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    diameter = Column(String(250)
+    population = Column(String(250), nullable=False)
+    terrain = Column(String(250), nullable=False)
+  
+class User(Base):
+    __tablename__ = 'user'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    mail= Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+
+class Favoritos(Base):
+    __tablename__ = 'planetas'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_id = Column(String(250)
+    planeta_id = Column(String(250), nullable=False)
+    persona_id = Column(String(250), nullable=False)
 
     def __repr__(self):
         return '<User %r>' % self.username
